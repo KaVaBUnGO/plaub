603cd3c711e363125de84e0cc5c3a29ab48800d3
KaVaBUnGO <kavabungo1@gmail.com>
2016-08-03 13:39:55 +0500

    init commit

---
+++ src/main/java/com/plaub/PlaubApplication.java
@@ -0,0 +1,12 @@
+package com.plaub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaubApplication.class, args);
+	}
+}
